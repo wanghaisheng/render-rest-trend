da65ea5ea147e7e13ca0070aa576d6fa47ae001a
wanghaisheng <edwin_uestc@163.com>
2024-12-07 14:13:44 +0800

    type

--- src/google-trends-api.d.ts
+++ src/google-trends-api.d.ts
@@ -23,14 +23,16 @@ declare module 'google-trends-api' {
     }
   
     // Define the structure of the Google Trends API functions
-    export default {
-      autoComplete: (options: { keyword: string }): Promise<AutoCompleteResult>;
-      dailyTrends: (options: { trendDate: Date; geo: string }): Promise<Trend[]>;
-      interestOverTime: (options: { keyword: string; startTime: Date }): Promise<InterestOverTime[]>;
-      interestByRegion: (options: { keyword: string; startTime: Date; geo: string }): Promise<any>;
-      realTimeTrends: (options: { geo: string }): Promise<any>;
-      relatedQueries: (options: { keyword: string }): Promise<RelatedQueries[]>;
-      relatedTopics: (options: { keyword: string }): Promise<any>;
+    const googleTrends: {
+      autoComplete(options: { keyword: string }): Promise<AutoCompleteResult>;
+      dailyTrends(options: { trendDate: Date; geo: string }): Promise<Trend[]>;
+      interestOverTime(options: { keyword: string; startTime: Date }): Promise<InterestOverTime[]>;
+      interestByRegion(options: { keyword: string; startTime: Date; geo: string }): Promise<any>;
+      realTimeTrends(options: { geo: string }): Promise<any>;
+      relatedQueries(options: { keyword: string }): Promise<RelatedQueries[]>;
+      relatedTopics(options: { keyword: string }): Promise<any>;
     };
+  
+    export default googleTrends;
   }
   
